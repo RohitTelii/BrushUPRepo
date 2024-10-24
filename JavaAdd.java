fdc3074adaae31a0d06773dd00f50f99da5eb56e
RohitTelii <rbteli8@gmail.com>
2024-10-24 11:40:27 +0530

    new changes added

--- JavaAdd.java
+++ JavaAdd.java
@@ -3,7 +3,7 @@ public class JavaAdd {
     public static void main(String[] args) {
         int a=4;
         int b=5;
-        int add = b +a;
+        int add = b -a;
         System.out.println("addiion is :" + add);
     }
 }
---
+++ MultiplyJava.java
@@ -0,0 +1,9 @@
+public class MultiplyJava {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int mul = b * a;
+        System.out.println("Multiplication is : " +mul );
+
+    }
+}
